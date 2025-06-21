bc9f17d3fc4d21eb606c7f0bd0e736de1253363f
necromante123 <henriqueg466700@gmail.com>
2025-06-21 15:06:26 -0300

    Atualizar o JsPDF.js
    Outfilers - Investigação Cibernética & Serviços Digitais
    
    Serviço rápido, sigiloso e acessível para rastreio de contas, localização de perfis fakes, investigações digitais, identificação por IP, recuperação de contas e muito mais.
    📌 Atendimento personalizado com Bruno Henrique.
    💰 Apenas R$ 40,00 via Pix.
    📲 Suporte direto via WhatsApp.

--- JsPDF.js
+++ JsPDF.js
@@ -1,5 +1,1 @@
-import { jsPDF } from "jspdf";
-
-const doc = new jsPDF();
-doc.text("Olá, este é um PDF gerado com jsPDF!", 10, 10);
-doc.save("Questionario.pdf");
+https://outfilers-netlify.netlify.app
